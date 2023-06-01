9ae05df8a59b44bb3a44d4767012bdcb4263c87c
HazaFG <hazaelfg@gmail.com>
2023-06-01 14:03:37 -0700

    Septimo Video

--- video_2.py
+++ video_2.py
@@ -1,6 +1,15 @@
-"""Probando los strings - VIDEO 6"""
+"""
+
+Probando los strings - VIDEO 6"
+
+"""
 
 print("Hola " + 'Hazael')
 print("Este texto es \"especial\"")
 print("\tHola \nHazael")
 print('This isn\'t a number') #No me marca error el apostrofe, puedo poner comilla doble o usar la barra invertida
+print("Mira escribo una barra \\")
+
+#Ejercicios
+print("Linea 1\n Linea 2\n Linea 3")
+print("A\tB\t\tC\nD\tE\t\tF\nG\tH\t\tI")
---
+++ video_7.py
@@ -0,0 +1,10 @@
+"""
+
+7. Input - VIDEO 7"
+
+"""
+
+#Input espera un ingreso
+
+print("Tu nombre es " +input ("Ingresa tu nombre: ") + " " + input("Dime tu apellido: "))
+
